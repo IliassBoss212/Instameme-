d1b383407952cd69b15e570293894fa957ecfa47
IliassBoss212 <167030403+IliassBoss212@users.noreply.github.com>
2024-04-14 21:59:39 +0200

    Add files via upload

---
+++ Instameme Final/script.js
@@ -0,0 +1,12 @@
+// Script pour la page d'accueil d'Instameme
+
+// Vous pouvez ajouter d'autres fonctionnalités ici, si nécessaire
+// Par exemple, charger dynamiquement les images à partir d'une source de données.
+
+// Attendez que le document soit prêt
+document.addEventListener("DOMContentLoaded", function () {
+    // Logique pour la page d'accueil
+    console.log("Page d'accueil chargée");
+
+    // Ajoutez d'autres opérations ici, si nécessaire
+});
